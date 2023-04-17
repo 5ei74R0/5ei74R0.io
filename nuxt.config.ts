31509312e9bfe3f2dac292f846a99eaf3f43e403
5ei74R0 <otoxukk.y@gmail.com>
2023-04-17 15:57:29 +0900

    [fix] favicon?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,11 +11,11 @@ export default {
         { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'A portfolio page of 5ei74R0' },
       ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "../5ei74R0.io/favicon.ico" },
+      ],
     },
   },
-  link: [
-    { rel: "icon", type: "image/x-icon", href: "/5ei74R0.io/favicon.ico" },
-  ],
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
