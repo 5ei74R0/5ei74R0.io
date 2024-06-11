c532e485b4bbeb0193bb920a90327f0dde0b5a47
5ei74R0 <otoxukk.y@gmail.com>
2024-06-11 13:05:23 +0900

    :adhesive_bandage: Fix bg issue on mobile

--- components/Palette.vue
+++ components/Palette.vue
@@ -2,28 +2,34 @@
   <div class="relative min-h-screen">
     <slot />
   </div>
-  </template>
-  
-  <style scoped>
+</template>
+
+<style scoped>
+.relative::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    z-index: -1000;
+}
+
+.relative::before {
+    background-image: url('/images/bg-waterfall4.png');
+}
+
+.dark .relative::before {
+    background-image: url('/images/bg-waterfall4-dark.png');
+}
+
+@media (max-width: 768px) {
   .relative::before {
-      content: "";
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      background-attachment: fixed;
-      z-index: -1000;
+    background-attachment: scroll;
   }
-  
-  .relative::before {
-  background-image: url('/images/bg-waterfall4.png');
-  }
-  
-  .dark .relative::before {
-  background-image: url('/images/bg-waterfall4-dark.png');
-  }
-  </style>
+}
+</style>
