ac53024aea6357d2f6da137bc39e4cde46a7ba6c
5ei74R0 <otoxukk.y@gmail.com>
2024-06-11 13:40:20 +0900

    :construction: Wip: tiny fix

--- components/Palette.vue
+++ components/Palette.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="flex flex-grow flex-col min-h-screen min-w-full 
+    class="flex flex-grow flex-col min-h-screen min-w-full scrollbar::-webkit-scrollbar-thumb
            bg-nord-snow-1 dark:bg-nord-night-1
-           md:bg-center md:bg-fixed md:bg-cover md:bg-[url('/images/bg-waterfall4.png')] md:dark:bg-[url('/images/bg-waterfall4-dark.png')] scrollbar::-webkit-scrollbar-thumb">
+           lg:bg-center lg:bg-fixed lg:bg-cover lg:bg-[url('/images/bg-waterfall4.png')] lg:dark:bg-[url('/images/bg-waterfall4-dark.png')]">
     <slot />
   </div>
 </template>
