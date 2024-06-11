b479b5d1b237d1b62dd007803d6767ccda2de35d
5ei74R0 <otoxukk.y@gmail.com>
2024-06-11 12:59:48 +0900

    :adhesive_bandage: Fix notation of before

--- components/Palette.vue
+++ components/Palette.vue
@@ -1,29 +1,29 @@
 <template>
-<div class="relative min-h-screen">
-  <slot />
-</div>
-</template>
-
-<style scoped>
-.relative:before {
-    content: "";
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    z-index: -1000;
-}
-
-.relative:before {
-background-image: url('/images/bg-waterfall4.png');
-}
-
-.dark .relative:before {
-background-image: url('/images/bg-waterfall4-dark.png');
-}
-</style>
+  <div class="relative min-h-screen">
+    <slot />
+  </div>
+  </template>
+  
+  <style scoped>
+  .relative::before {
+      content: "";
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      z-index: -1000;
+  }
+  
+  .relative::before {
+  background-image: url('/images/bg-waterfall4.png');
+  }
+  
+  .dark .relative::before {
+  background-image: url('/images/bg-waterfall4-dark.png');
+  }
+  </style>
